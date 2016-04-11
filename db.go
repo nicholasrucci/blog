c813e00e5725a9a115a1724cefa35e9d6dc4f095
Nick Rucci <ld.tech@outlook.com>
2016-04-11 17:00:55 -0600

    Add database skeleton

---
+++ db.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+
+	"log"
+)
+
+func dbConnection() {
+	db, err := sql.Open("mysql",
+		"user:password@tcp(127.0.0.1:3306)/hello")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+}
