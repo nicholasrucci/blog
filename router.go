e040a325feaca054ba2765621ec342ea6f2561f3
Nick Rucci <ld.tech@outlook.com>
2016-04-12 13:48:56 -0600

    Add documentation comments to methods

--- router.go
+++ router.go
@@ -6,6 +6,9 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// NewRouter is creating the router using that
+// is being used throughout the project using
+// gorilla mux
 func NewRouter() *mux.Router {
 	// create new router
 	router := mux.NewRouter()
@@ -27,6 +30,8 @@ func NewRouter() *mux.Router {
 
 }
 
+// JSONHandler is being put in all of the handler functions to
+// make the Content Type of the responses as JSON.
 func JSONHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
