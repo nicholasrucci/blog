827dafaed7153320051ac29d8de2b43c374fe1f4
Nick Rucci <ld.tech@outlook.com>
2016-04-12 15:16:27 -0600

    Remove return value

--- status.go
+++ status.go
@@ -12,21 +12,17 @@ type StatusResponse struct {
 }
 
 // SuccessStatus will send a 200 success status code
-func SuccessStatus(w http.ResponseWriter, r *http.Request) error {
+func SuccessStatus(w http.ResponseWriter, r *http.Request) {
 	res := StatusResponse{200, "The request was fulfilled."}
 	if err := json.NewEncoder(w).Encode(res); err != nil {
 		log.Fatal(err)
-		return err
 	}
-	return nil
 }
 
 // ErrorStatus will send a 500 unexpected condition status code
-func ErrorStatus(w http.ResponseWriter, r *http.Request) error {
+func ErrorStatus(w http.ResponseWriter, r *http.Request) {
 	res := StatusResponse{500, "The server encountered an unexpected condition which prevented it from fulfilling the request."}
 	if err := json.NewEncoder(w).Encode(res); err != nil {
 		log.Fatal(err)
-		return err
 	}
-	return nil
 }
