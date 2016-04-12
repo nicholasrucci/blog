256f72812cf6ee714e0145bd66e906b61078373d
Nick Rucci <ld.tech@outlook.com>
2016-04-12 15:15:17 -0600

    Comment documentation

--- status.go
+++ status.go
@@ -11,6 +11,7 @@ type StatusResponse struct {
 	Message    string
 }
 
+// SuccessStatus will send a 200 success status code
 func SuccessStatus(w http.ResponseWriter, r *http.Request) error {
 	res := StatusResponse{200, "The request was fulfilled."}
 	if err := json.NewEncoder(w).Encode(res); err != nil {
@@ -20,6 +21,7 @@ func SuccessStatus(w http.ResponseWriter, r *http.Request) error {
 	return nil
 }
 
+// ErrorStatus will send a 500 unexpected condition status code
 func ErrorStatus(w http.ResponseWriter, r *http.Request) error {
 	res := StatusResponse{500, "The server encountered an unexpected condition which prevented it from fulfilling the request."}
 	if err := json.NewEncoder(w).Encode(res); err != nil {
