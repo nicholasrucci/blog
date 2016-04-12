32693c7e2ff08337143ffd5f71ccb03e8f0166d6
Nick Rucci <ld.tech@outlook.com>
2016-04-12 15:44:20 -0600

    Use environment variables

--- db.go
+++ db.go
@@ -5,13 +5,13 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 
 	"log"
+	"os"
 )
 
 // dbConnection opens a new database connection and
 // returns a pointer to the sql.DB
 func dbConnection() *sql.DB {
-	db, err := sql.Open("mysql",
-		"root:@tcp(127.0.0.1:3306)/blog")
+	db, err := sql.Open("mysql", os.Getenv("DB"))
 	if err != nil {
 		log.Fatal(err)
 	}
