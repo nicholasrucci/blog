b7fc7bba464e93f3ee72f2e52322793404633f65
Nick Rucci <ld.tech@outlook.com>
2016-04-12 13:05:32 -0600

    Remove parsing on time

--- db.go
+++ db.go
@@ -9,7 +9,7 @@ import (
 
 func dbConnection() *sql.DB {
 	db, err := sql.Open("mysql",
-		"root:@tcp(127.0.0.1:3306)/blog?parseTime=true")
+		"root:@tcp(127.0.0.1:3306)/blog")
 	if err != nil {
 		log.Fatal(err)
 	}
