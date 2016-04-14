51f59c22a38b2b08642e7a3ba1da3b0c204408e1
Nick Rucci <ld.tech@outlook.com>
2016-04-13 20:15:47 -0600

    Set up basic card for pulled in data

--- public/ajaxRequest.js
+++ public/ajaxRequest.js
@@ -1,5 +1,3 @@
-/* eslint no-empty-label: 0 */
-
 'use strict';
 
 (function() {
@@ -20,21 +18,35 @@
         console.log(posts);
 
         for (i = 0; i < posts.length; i++) {
-          var div         = document.createElement("div");
-          var link        = document.createElement("a")
-          var title       = document.createElement("h2");
+          var card        = document.createElement("div");
+          var cardContent = document.createElement("div");
+          var cardAction  = document.createElement("div");
+          var link        = document.createElement("a");
+          var title       = document.createElement("span");
           var titleText   = document.createTextNode(posts[i].title);
           var content     = document.createElement("p");
           var contentText = document.createTextNode(posts[i].content);
           var element     = document.getElementById("posts-container");
 
-          div.appendChild(link)
-          link.appendChild(title)
-          link.setAttribute('href',"#")
+          card.appendChild(cardContent)
+          card.className += "card blue-grey darken-1";
+
+          cardContent.appendChild(title)
+          cardContent.className += "card-content white-text";
+          cardContent.appendChild(content)
+          cardContent.appendChild(cardAction);
+
+          cardAction.className += "card-action";
+          cardAction.appendChild(link);
+
+          link.innerHTML = "Click to Read";
+
           title.appendChild(titleText);
-          div.appendChild(content)
+          title.className += "card-title";
+
           content.appendChild(contentText)
-          element.appendChild(div);
+
+          element.appendChild(card);
         }
       }
     }
