3285bae933cb15b6c8cef4cbdb8b8fa69ec6462b
Nick Rucci <ld.tech@outlook.com>
2016-04-14 21:36:23 -0600

    Set up admin form to upload a post

---
+++ public/javascripts/createAjaxRequest.js
@@ -0,0 +1,21 @@
+'use strict';
+
+  function send() {
+    var xhr = new XMLHttpRequest();
+    var title;
+    var content;
+    var reqObject;
+
+    title   = document.getElementById("title").value;
+    content = document.getElementById("content").value
+
+    reqObject = {
+      "title": title,
+      "content": content
+    }
+
+    xhr.open("POST", "/api/posts");
+    xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+    xhr.send(JSON.stringify(reqObject));
+    window.location.replace("/");
+  }
