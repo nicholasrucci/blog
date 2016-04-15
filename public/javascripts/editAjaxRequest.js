8939d88291df77b1f8ec2e5d19d9d09a0fab170d
Nick Rucci <ld.tech@outlook.com>
2016-04-15 14:08:35 -0600

    implement update ajax req

--- public/javascripts/editAjaxRequest.js
+++ public/javascripts/editAjaxRequest.js
@@ -1,35 +1,53 @@
 'use strict';
 
-(function() {
+var xhr = new XMLHttpRequest();
+var postID = window.location.href.split('/');
+
+postID = postID[postID.length - 2];
+
+xhr.open('GET', '/api/posts/' + postID);
+xhr.send(null);
+
+xhr.onreadystatechange = function() {
+  var DONE = 4; // readyState 4 means the request is done.
+  var OK = 200; // status 200 is a successful return.
+  var post, posts, i;
+
+  if (xhr.readyState === DONE) {
+    if (xhr.status === OK) {
+
+      post = JSON.parse(xhr.responseText);
+
+      var title   = document.getElementById("title");
+      var content = document.getElementById("content");
+
+      title.setAttribute("value", post.title);
+      content.innerHTML = post.content;
+
+    }
+  }
+};
+
+function send() {
   var xhr = new XMLHttpRequest();
+  var title;
+  var content;
+  var reqObject;
+  var postID;
+
   var postID = window.location.href.split('/');
-
-  console.log(postID);
-
   postID = postID[postID.length - 2];
 
-  console.log(postID);
+  title   = document.getElementById("title").value;
+  content = document.getElementById("content").value
 
-  xhr.open('GET', '/api/posts/' + postID);
-  xhr.send(null);
+  reqObject = {
+    "title": title,
+    "content": content
+  }
 
-  xhr.onreadystatechange = function() {
-    var DONE = 4; // readyState 4 means the request is done.
-    var OK = 200; // status 200 is a successful return.
-    var post, posts, i;
-
-    if (xhr.readyState === DONE) {
-      if (xhr.status === OK) {
-
-        post = JSON.parse(xhr.responseText);
-
-        var title   = document.getElementById("title");
-        var content = document.getElementById("content");
-
-        title.setAttribute("value", post.title);
-        content.innerHTML = post.content;
-
-      }
-    }
-  };
-})();
+  xhr.open("PUT", "/api/posts/" + postID);
+  xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+  xhr.send(JSON.stringify(reqObject));
+  window.location.replace("/posts/" + postID);
+}
