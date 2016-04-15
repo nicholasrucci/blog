10fb3e82a8e03b81fa45ba8b14306c21ddb2bda1
Nick Rucci <ld.tech@outlook.com>
2016-04-15 13:55:07 -0600

    grab content

---
+++ public/javascripts/editAjaxRequest.js
@@ -0,0 +1,30 @@
+'use strict';
+
+(function() {
+  var xhr = new XMLHttpRequest();
+  var postID = window.location.href.split('/');
+
+  console.log(postID);
+
+  postID = postID[postID.length - 2];
+
+  console.log(postID);
+
+  xhr.open('GET', '/api/posts/' + postID);
+  xhr.send(null);
+
+  xhr.onreadystatechange = function() {
+    var DONE = 4; // readyState 4 means the request is done.
+    var OK = 200; // status 200 is a successful return.
+    var post, posts, i;
+
+    if (xhr.readyState === DONE) {
+      if (xhr.status === OK) {
+
+        post = JSON.parse(xhr.responseText);
+        console.log(post);
+
+      }
+    }
+  };
+})();
